3b3e1b948e3f93fa3467852691b5eeb29cdaddec
Enrique Mejía <mejiasantiagoenrique@gmail.com>
2025-12-10 22:33:20 -0600

    fix: Change API_URL to call my microservice deployed in Render.

--- local_scripts/client.py
+++ local_scripts/client.py
@@ -1,17 +1,19 @@
 import requests, json
 
-# Parameters, getting the environment variables
-IP = "192.168.1.224" # Second value is the default if not found 
-PORT = "8111"
-API_URL = f'http://{IP}:{PORT}/api_predict_income'
+# Parameters
+#IP = "192.168.1.224"
+#PORT = "8111"
+RENDER_URL = 'https://simple-model-deployment-flask.onrender.com'
+API_URL = f'{RENDER_URL}/api_predict_income'
 
 # Data to send
 # education = 0:No degree, 1:Bachelors, 2:Masters, 3:Doctorate
 #years_of_experience = Years of Experience as integer
 #etam_members = 3  # Number of people in their team (direct and indirect reports)
 data_to_send = [
-    {"education": 1, "years_of_experience": 10, "team_members": 50},
-    {"education": 2, "years_of_experience": 30, "team_members": 1}
+    {"education": 0, "years_of_experience": 0, "team_members": 0},
+    {"education": 2, "years_of_experience": 10, "team_members": 20},
+    {"education": 3, "years_of_experience": 20, "team_members": 60}
                 ]
 
 # Sending request and receiving prediction
